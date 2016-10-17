2b4f16cdb32bca5785a05a60d853d1f8433b574c
Eric <men15002@uvg.edu.gt>
2016-10-17 17:30:45 -0600

    Solo se ha agregado la clase de redBlackTree

---
+++ src/MapFactory.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+/**
+ * Esta clase devuelve un Map en cualquiera de sus dos variedades
+ * @author Eric Mendoza 15002
+ * @author Jonnathan Juarez 15377
+ * @since 16/10/2016
+ * @version 1.0
+ */
+public class MapFactory {
+    /**
+     * Metodo que devuelve un stack tipo map segun lo indicado por el usuario
+     * @param map Es el tipo de map que se desea
+     * @return Devuelve un map
+     */
+    <E> Stack<E> getMap(String map) {
+
+        return null;
+    }
+}
